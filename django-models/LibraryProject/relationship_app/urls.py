836ccec83dabb59042517c5185ff9ee635f1dee0
JeanNepomuscene <nano.threetena@gmail.com>
2025-09-05 14:52:27 +0200

    Add files via upload

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -3,6 +3,7 @@
 from django.urls import path
 from . import views
 from django.contrib.auth.views import LoginView, LogoutView
+from .views import list_books
 
 urlpatterns = [
     # Books and libraries
