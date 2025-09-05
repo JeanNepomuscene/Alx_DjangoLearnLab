7fd76d028bef806925c5c134f7f815b3cde784fc
JeanNepomuscene <nano.threetena@gmail.com>
2025-09-05 13:13:51 +0200

    Add files via upload

---
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -0,0 +1,13 @@
+from relationship_app.models import Author, Book, Library, Librarian
+
+
+def get_books_by_author(author_name):
+    return Book.objects.filter(author__name=author_name)
+
+
+def get_books_in_library(library_name):
+    return Library.objects.get(name=library_name).books.all()
+
+
+def get_librarian_for_library(library_name):
+    return Library.objects.get(name=library_name).librarian
