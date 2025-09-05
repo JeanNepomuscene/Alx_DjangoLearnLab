af00cb982b962a6a37a49ea6da245d55e89a91f1
JeanNepomuscene <nano.threetena@gmail.com>
2025-09-05 13:39:57 +0200

    Add files via upload

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -3,13 +3,10 @@ from django.views.generic import DetailView
 from .models import Book
 from .models import Library   # 👈 ALX checker wants this exact line
 
-
-
 # Function-based view: list all books
 def list_books(request):
     books = Book.objects.all()
     return render(request, "relationship_app/list_books.html", {"books": books})
-
 
 # Class-based view: details for a specific library
 class LibraryDetailView(DetailView):
