5df0703b42c3955cea4ac2247fd46adb3d72025a
JeanNepomuscene <nano.threetena@gmail.com>
2025-09-05 14:31:55 +0200

    Add files via upload

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -58,20 +58,22 @@ def member_view(request):
     return render(request, 'relationship_app/member_view.html')
 
 # -------------------------------
-# Book Management Views with Permissions
+# Permission-Protected Book Views
 # -------------------------------
+@login_required
 @permission_required('relationship_app.can_add_book', raise_exception=True)
 def add_book(request):
     if request.method == 'POST':
         title = request.POST.get('title')
         author_id = request.POST.get('author')
         if title and author_id:
-            author = get_object_or_404(Author, pk=author_id)
+            author = get_object_or_404(Author, id=author_id)
             Book.objects.create(title=title, author=author)
             return redirect('list_books')
     authors = Author.objects.all()
     return render(request, 'relationship_app/add_book.html', {'authors': authors})
 
+@login_required
 @permission_required('relationship_app.can_change_book', raise_exception=True)
 def edit_book(request, pk):
     book = get_object_or_404(Book, pk=pk)
@@ -79,14 +81,14 @@ def edit_book(request, pk):
         title = request.POST.get('title')
         author_id = request.POST.get('author')
         if title and author_id:
-            author = get_object_or_404(Author, pk=author_id)
             book.title = title
-            book.author = author
+            book.author = get_object_or_404(Author, id=author_id)
             book.save()
             return redirect('list_books')
     authors = Author.objects.all()
     return render(request, 'relationship_app/edit_book.html', {'book': book, 'authors': authors})
 
+@login_required
 @permission_required('relationship_app.can_delete_book', raise_exception=True)
 def delete_book(request, pk):
     book = get_object_or_404(Book, pk=pk)
