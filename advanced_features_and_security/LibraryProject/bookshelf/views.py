20281c41cc556f3c35d9daa19cfc33e3fddc683b
JeanNepomuscene <nano.threetena@gmail.com>
2025-09-13 08:16:01 +0200

    Update views.py

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,34 +1,9 @@
-# advanced_features_and_security/views.py
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import render
+from .models import Book
 from django.contrib.auth.decorators import permission_required
-from .models import Article
 
-@permission_required('advanced_features_and_security.can_view', raise_exception=True)
-def article_list(request):
-    articles = Article.objects.all()
-    return render(request, "articles/list.html", {"articles": articles})
-
-@permission_required('advanced_features_and_security.can_create', raise_exception=True)
-def article_create(request):
-    if request.method == "POST":
-        title = request.POST.get("title")
-        content = request.POST.get("content")
-        Article.objects.create(title=title, content=content, author=request.user)
-        return redirect("article_list")
-    return render(request, "articles/create.html")
-
-@permission_required('advanced_features_and_security.can_edit', raise_exception=True)
-def article_edit(request, pk):
-    article = get_object_or_404(Article, pk=pk)
-    if request.method == "POST":
-        article.title = request.POST.get("title")
-        article.content = request.POST.get("content")
-        article.save()
-        return redirect("article_list")
-    return render(request, "articles/edit.html", {"article": article})
-
-@permission_required('advanced_features_and_security.can_delete', raise_exception=True)
-def article_delete(request, pk):
-    article = get_object_or_404(Article, pk=pk)
-    article.delete()
-    return redirect("article_list")
+# List all books (requires can_view permission)
+@permission_required('bookshelf.can_view', raise_exception=True)
+def book_list(request):
+    books = Book.objects.all()  # fetch all books
+    return render(request, "bookshelf/book_list.html", {"books": books})
